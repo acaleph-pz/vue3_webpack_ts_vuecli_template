d4e07fc1cd7808ad0b957f2c4025aa47ffe34eb0
Ronin <2254314552@qq.com>
2022-07-13 17:16:06 +0800

    init

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,25 +1,17 @@
-import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import type { RouteRecordRaw } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: () => import('~/pages/sys/home/home.vue'),
   },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
 ]
 
 const router = createRouter({
   history: createWebHashHistory(),
-  routes
+  routes,
 })
 
 export default router
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
+import path from 'path'
 const { defineConfig } = require('@vue/cli-service')
+const resolve = (dir) => {
+  return path.join(__dirname, dir)
+}
 module.exports = defineConfig({
+  chainWebpack: (config) => {
+    config.resolve.alias.set('~', resolve('src'))
+  },
   transpileDependencies: true,
 })
